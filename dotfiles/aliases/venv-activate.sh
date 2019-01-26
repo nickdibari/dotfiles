5945197196fb6bb9102e9b4e601309ca142a07c9
nickdibari <ndibari@fordham.edu>
2019-01-26 01:28:46 -0500

    First passing version of venv completion

--- dotfiles/aliases/venv-activate.sh
+++ dotfiles/aliases/venv-activate.sh
@@ -1,9 +1,19 @@
+#! /bin/sh
+
 # Alias for activating a virtual environemt when `cd` into directory
 # - This command assumes the virtual environment lives in the venv/ directory
 # - You can configure this to your usual virtual environment practices
+# - Set $venv_name to whatever name you normally use for setting up virtual environment
+# - Deactivates virtual environment after leaving directory
+
+venv_name="venv"
+
 function cd {
     builtin cd "$@"
-    if [ -d "venv" ]; then
-        source venv/bin/activate
+    if [[ -d $venv_name ]]; then
+        source $venv_name/bin/activate
+
+    elif [[ ! -z $VIRTUAL_ENV ]] ; then
+        deactivate
     fi
 }

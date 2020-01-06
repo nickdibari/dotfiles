612490c55441bcf7621943cc5e40e02720853102
nickdibari <ndibari@fordham.edu>
2020-01-06 00:17:12 -0500

    Use find command to look for python virtual environment

--- dotfiles/aliases/venv-activate.sh
+++ dotfiles/aliases/venv-activate.sh
@@ -1,19 +1,17 @@
 #! /bin/sh
 
-# Alias for activating a virtual environemt when `cd` into directory
-# - This command assumes the virtual environment lives in the venv/ directory
-# - You can configure this to your usual virtual environment practices
-# - Set $venv_name to whatever name you normally use for setting up virtual environment
+# Alias for activating a virtual environment when moving into a directory
 # - Deactivates virtual environment after leaving directory
-
-venv_name="venv"
 
 function cd {
     builtin cd "$@"
-    if [[ -d $venv_name ]]; then
-        source $venv_name/bin/activate
 
-    elif [[ ! -z $VIRTUAL_ENV ]] ; then
+    venv_source=$(find . -maxdepth 3 -name activate 2>/dev/null)
+
+    if [[ ! -z $venv_source ]]; then
+        source $venv_source
+
+    elif [[ ! -z $VIRTUAL_ENV ]]; then
         deactivate
     fi
 }

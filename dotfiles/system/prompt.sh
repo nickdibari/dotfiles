9d5ebad3deec382d000c90bff63fe57c2e140904
nickdibari <ndibari@fordham.edu>
2020-10-01 21:33:58 -0400

    Set full working directory for prompt display

--- dotfiles/system/prompt.sh
+++ dotfiles/system/prompt.sh
@@ -8,7 +8,7 @@ case "$TERM" in
         PS1='${debian_chroot:+($debian_chroot)}\[\033[01;${UCOLOR}m\]\u@\[\033[01;${HCOLOR}m\]\h\[\033[00m\]:\!:\[\033[00m\]\$'
         PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
         [ $TERM == "screen" ] && export PS1="\[\033k\033\134\033k\h\033\134\]$PS1"
-        PS1="\[\033[00;37m\][\T]:\[\033[00;36m\]\W\$(__git_ps1)\n$PS1 "
+        PS1="\[\033[00;37m\][\T]:\[\033[00;36m\]\w\$(__git_ps1)\n$PS1 "
         ;;
     *)
         ;;

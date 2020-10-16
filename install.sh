5fe6ce924e9c952f98b00b30857ad39bbbe0a5a6
nickdibari <ndibari@fordham.edu>
2020-10-16 19:05:00 -0400

    Quote variables in install script

--- install.sh
+++ install.sh
@@ -7,40 +7,38 @@ MAIN_DIRECTORY="$HOME/.dotfiles"
 MAIN_FILENAME="$MAIN_DIRECTORY/.activate"
 BASH_FILE="$HOME/.bashrc"
 
-if [[ -d $MAIN_DIRECTORY ]]; then
+if [[ -d "$MAIN_DIRECTORY" ]]; then
     echo "Clearing old install.."
-    rm -rf $MAIN_DIRECTORY
+    rm -rf "$MAIN_DIRECTORY"
 fi
 
-mkdir $MAIN_DIRECTORY
-touch $MAIN_FILENAME
+mkdir "$MAIN_DIRECTORY"
+touch "$MAIN_FILENAME"
 
 dotfiles_dirs=$( ls "dotfiles")
 
 for dir in $dotfiles_dirs; do
-    dotfiles=$(ls dotfiles/$dir)
+    dotfiles=$(ls dotfiles/"$dir")
 
     for file in $dotfiles; do
         filename="dotfiles/$dir/$file"
 
-        cp $filename $MAIN_DIRECTORY
+        cp "$filename" "$MAIN_DIRECTORY"
         new_filename="$MAIN_DIRECTORY/$file"
 
         echo "Appending $new_filename to $MAIN_FILENAME"
-        echo ". $new_filename" >> $MAIN_FILENAME
+        echo ". $new_filename" >> "$MAIN_FILENAME"
     done
 done
 
 echo "Copying your $BASH_FILE file to /tmp in case of revert"
-cp ${BASH_FILE} /tmp/bashrc.bak
+cp "${BASH_FILE}" /tmp/bashrc.bak
 
 echo "Appending $MAIN_FILENAME to user $BASH_FILE"
-echo "" >> $BASH_FILE
-echo "### BEGIN dotfiles MANAGED SECTION" >> $BASH_FILE
-echo ". $MAIN_FILENAME" >> $BASH_FILE
-echo '### END dotfiles MANAGED SECTION' >> $BASH_FILE
+echo "" >> "$BASH_FILE"
+echo "### BEGIN dotfiles MANAGED SECTION" >> "$BASH_FILE"
+echo ". $MAIN_FILENAME" >> "$BASH_FILE"
+echo '### END dotfiles MANAGED SECTION' >> "$BASH_FILE"
 
 echo "Sourcing $BASH_FILE"
-. $BASH_FILE
-
-
+source "$BASH_FILE"

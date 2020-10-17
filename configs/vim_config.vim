864e67498e0e59dac3e3d7319cd5559ce6a9a42a
nickdibari <ndibari@fordham.edu>
2020-10-16 21:32:46 -0400

    Add custom vim config to install

---
+++ configs/vim_config.vim
@@ -0,0 +1,5 @@
+" Trim trailing whitespace on save
+autocmd BufWritePre * :%s/\s\+$//e
+
+" Add command for writing files with `sudo`
+command W :execute ':silent W !sudo tee "%" > /dev/null' | :edit!
--- install.bash
+++ install.bash
@@ -45,3 +45,7 @@ echo '### END dotfiles MANAGED SECTION' >> "$BASH_FILE"
 echo "Installing awesome vim config"
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# Add custom vim config
+echo "Installing custom vim config"
+cp configs/vim_config.vim ~/.vim_runtime/my_configs.vim
